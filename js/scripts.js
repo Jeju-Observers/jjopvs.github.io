7a0a90c87993c99181a7ab1613311f75ca493511
lynnjean <kimlynnjean@naver.com>
2024-08-29 16:23:22 +0900

    🐛 지도 버그 수정

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
-// 지도를 표시할 div와 지도 옵션 설정
-var mapContainer = document.getElementById("map"),
-  mapOption = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-    level: 3, // 지도의 확대 레벨
-    mapTypeId: kakao.maps.MapTypeId.HYBRID, // kakao.maps.MapTypeId.SKYVIEW : 주소 없이 표시
-  };
-
-// 지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption);
+document.addEventListener("DOMContentLoaded", function () {
+  if (typeof kakao !== "undefined" && kakao.maps) {
+    kakao.maps.load(function () {
+      var mapContainer = document.getElementById("map");
+      var mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+        mapTypeId: kakao.maps.MapTypeId.HYBRID, // kakao.maps.MapTypeId.SKYVIEW : 주소 없이 표시
+      };
+      var map = new kakao.maps.Map(mapContainer, mapOption);
+    });
+  } else {
+    console.error("Kakao maps API is not loaded.");
+  }
+});
